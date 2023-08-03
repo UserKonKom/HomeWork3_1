d0ec72562df3971ffed2391565a9978f48f2da95
Konstantin <lolkup705@gmail.com>
2023-08-03 17:35:51 +0900

    add HW 6

--- HW_6/Program.cs
+++ HW_6/Program.cs
@@ -5,3 +5,16 @@
 7 -> нет
 */
 
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0)
+{
+    Console.WriteLine("Число " + num + " четное?");
+    Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("Число " + num + " четное?");
+    Console.WriteLine("НЕТ!");
+}
